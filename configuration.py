169001d9c01ca23ea290a9f6b4c01164009e15c1
Merit Calderon Virgilio <meritcalderonv@gmail.com>
2025-10-20 16:13:32 -0700

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-8209acc8-4853-46ea-8beb-1cc09e125a38.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
